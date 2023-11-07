70b57b12ec86c40dfe2955b1885937a4a96a3c61
NapalmZ <willy.baessato@gmail.com>
2023-11-07 09:18:25 +0100

    Addedo double support influx 1 and 2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:2-alpine
+#FROM python:2-alpine
+FROM python:alpine3.17
 LABEL maintainer="NapalmZ (https://github.com/napalmz)"
 
 ENV LANG=C.UTF-8
@@ -10,6 +11,6 @@ RUN apk --no-cache update \
         tzdata
 
 COPY ./apcupsd-influxdb-exporter.py /apcupsd-influxdb-exporter.py
-RUN pip install apcaccess influxdb
+RUN pip install apcaccess influxdb influxdb-client
 
 CMD ["python", "/apcupsd-influxdb-exporter.py"]
--- apcupsd-influxdb-exporter.py
+++ apcupsd-influxdb-exporter.py
@@ -3,33 +3,57 @@ import os
 import time
 
 from apcaccess import status as apc
-from influxdb import InfluxDBClient
 
-dbname    = os.getenv('INFLUXDB_DATABASE', 'apcupsd')
-user      = os.getenv('INFLUXDB_USER')
-password  = os.getenv('INFLUXDB_PASSWORD')
-port      = os.getenv('INFLUXDB_PORT', 8086)
-host      = os.getenv('INFLUXDB_HOST', '10.0.1.9')
-interval  = float(os.getenv('INTERVAL', 5))
-client    = InfluxDBClient(host, port, user, password, dbname)
-ups_alias = os.getenv('UPS_ALIAS','none')
+# Client v1
+import influxdb
+# Client v2
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-client.create_database(dbname)
+# Influxdb choosing
+dbversion    = int(os.getenv('INFLUXDB_VERSION', '2'))
+# Influxdb 1
+v1_host      = os.getenv('INFLUXDB1_HOST', 'influxdb')
+v1_port      = os.getenv('INFLUXDB1_PORT', 8086)
+v1_user      = os.getenv('INFLUXDB1_USER')
+v1_password  = os.getenv('INFLUXDB1_PASSWORD')
+v1_dbname    = os.getenv('INFLUXDB1_DATABASE', 'apcupsd')
+# Influxdb 2
+v2_url       = os.getenv('INFLUXDB2_URL', 'http://influxdb:8086')
+v2_token     = os.getenv('INFLUXDB2_TOKEN', 'my-token')
+v2_org       = os.getenv('INFLUXDB2_ORG', 'my-org')
+v2_bucket    = os.getenv('INFLUXDB2_BUCKET', 'my-bucket')
+# Other configs
+interval     = float(os.getenv('INTERVAL', 5))
+ups_alias    = os.getenv('UPS_ALIAS','none')
+apcupsd_host = os.getenv('APCUPSD_HOST', 'localhost')
+verbose      = os.getenv('VERBOSE', 'false').lower()
+
+if (dbversion == 1):
+    client = influxdb.InfluxDBClient(v1_host, v1_port, v1_user, v1_password, v1_dbname)
+    client.create_database(v1_dbname)
+    print("INFLUXDB1_HOST: ", v1_host)
+    print("INFLUXDB1_PORT: ", v1_port)
+    print("INFLUXDB1_USER: ", v1_user)
+    print("INFLUXDB1_PASSWORD: [redacted]")
+    print("INFLUXDB1_DATABASE: ", v1_dbname)
+elif (dbversion == 2):
+    client = influxdb_client.InfluxDBClient(url=v2_url, token=v2_token, org=v2_org)
+    #client.create_database(dbname)
+    print("INFLUXDB2_URL: ", v2_url)
+    print("INFLUXDB2_TOKEN: [redacted]")
+    print("INFLUXDB2_ORG: ", v2_org)
+    print("INFLUXDB2_BUCKET: ", v2_bucket)
 
 # Print envs
-print("INFLUXDB_DATABASE: ", dbname)
-print("INFLUXDB_USER: ", user)
-print("INFLUXDB_PASSWORD: ", password)
-print("INFLUXDB_PORT: ", port)
-print("INFLUXDB_HOST: ", host)
 print("UPS_ALIAS", ups_alias)
 print("INTERVAL: ", interval)
-print("APCUPSD_HOST", os.getenv('APCUPSD_HOST', 'localhost'))
-print("VERBOSE: ", os.getenv('VERBOSE', 'localhost'))
+print("APCUPSD_HOST", apcupsd_host)
+print("VERBOSE: ", verbose)
 
 while True:
     try:
-        ups = apc.parse(apc.get(host=os.getenv('APCUPSD_HOST', 'localhost')), strip_units=True)
+        ups = apc.parse(apc.get(host=apcupsd_host), strip_units=True)
         watts = float(os.getenv('WATTS', ups.get('NOMPOWER', 0.0))) * 0.01 * float(ups.get('LOADPCT', 0.0))
         json_body = [
             {
@@ -54,11 +78,17 @@ while True:
                 }
             }
         ]
-        if os.getenv('VERBOSE', 'false').lower() == 'true':
+        if verbose == 'true':
             print(json_body)
-            print(client.write_points(json_body))
+            if (dbversion == 1):
+                print(client.write_points(json_body))
+            elif (dbversion == 2):
+                print(client.write_api(write_options=SYNCHRONOUS).write(bucket=v2_bucket, record=json_body))
         else:
-            client.write_points(json_body)
-    except:
-        print('General error, retry in seconds...')
+            if (dbversion == 1):
+                client.write_points(json_body)
+            elif (dbversion == 2):
+                client.write_api(write_options=SYNCHRONOUS).write(bucket=v2_bucket, record=json_body)
+    except Exception as e:
+        print('General error, retry in seconds...', e)
     time.sleep(interval)
